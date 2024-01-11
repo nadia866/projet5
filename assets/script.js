1598c6fb0824bc44c6c749224b849f8a48798c92
Nadia Touchna <nadia.touchna@gmail.com>
2024-01-11 11:56:12 +0100

    modif code html et ajout code sur javascript pour flèches

--- assets/script.js
+++ assets/script.js
@@ -1,48 +1,45 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+		"image": "slide1.jpg",
+		"tagLine": "Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+		"image": "slide2.jpg",
+		"tagLine": "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+		"image": "slide3.jpg",
+		"tagLine": "Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+		"image": "slide4.png",
+		"tagLine": "Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+//Étape 2 : Ajoutez des Event Listeners sur les flèches 
+const bannerImg = document.querySelector('.banner-img');
+const arrowLeft = document.querySelector('.arrow_left');
+const arrowRight = document.querySelector('.arrow_right');
+const dots = document.querySelectorAll('.dot'); // Sélections de tous les points
 
-// Ajout d'event listeners sur les flèches
-let bannerImg = document.querySelector('.banner-img');
-let arrowLeft = document.querySelector(".arrow_left");
-let arrowRight = document.querySelector(".arrow_right");
-
-// Event listener pour la flèche gauche
-arrowLeft.addEventListener("click", function() {
-    console.log("Clic sur la flèche gauche");
-});
-
-// Event listener pour la flèche droite
-arrowRight.addEventListener("click", function() {
-    console.log("Clic sur la flèche droite");
-});
-
-//bullets points
-const dots = document.querySelectorAll('.dot'); // Sélectionnez tous les points
 let currentIndex = 0;
 
-// Fonction pour mettre à jour les points indicateurs
+// Étape 3 : Ajoutez des bullet points au slider
 function updateDots(index) {
     dots.forEach((dot, i) => {
-        if (i === index) {
-            dot.classList.add('dot_selected'); // Ajoutez la classe pour le point actuel
-        } else {
-            dot.classList.remove('dot_selected'); // Supprimez la classe pour les autres points
-        }
+        dot.classList.toggle('dot_selected', i === index);
     });
 }
+// Fonction pour mettre à jour le carrousel
+function updateCarousel(index, direction) {
+    currentIndex = (index + slides.length) % slides.length;
+
+    const { image, tagLine } = slides[currentIndex];
+
+    bannerImg.src = `assets/images/slideshow/${image}`;
+    bannerImg.alt = `Slide ${currentIndex + 1}`;
+    document.querySelector('p').innerHTML = tagLine;
+
+    console.log(`Clic sur la flèche ${direction}`);
+}
+
